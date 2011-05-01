a2af5269f6ee32a2baeb40c7d6ba1d76cf39894a
Masud Khan <masudio@gmail.com>
2011-04-30 19:31:21 -0600

    added interface file SuffixHelper.cs

---
+++ Periodicity Detection (Complexity Improvement)/SuffixHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Periodicity_Detection__Complexity_Improvement_ {
+    interface SuffixHelper
+    {
+        /**
+        * returns true if the given substring exists in this object's tree, false
+        * otherwise.
+        */
+        bool FindSubstring(string theSubstring);
+
+        /**
+         * Returns a list of all occurrences of the given substring, or an empty list if
+         * none are found.
+         */
+        IEnumerable<int> FindAllOccurrences(string theSubstring);
+    }
+}
